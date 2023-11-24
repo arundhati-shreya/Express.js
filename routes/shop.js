fde78e1d82ccb6180b0b383dbc89eefd4c5bb26d
arundhati-shreya <arundhati0426@gmail.com>
2023-11-24 11:24:13 +0530

    Controllers

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,12 @@
-const path=require('path');
 
-const express=require('express');
-const rootDir=require('../util/path');
+const path = require('path');
 
-const router=express.Router();
+const express = require('express');
 
-router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views/shop.html'));
-});
+const productsController = require('../controllers/products');
 
-module.exports=router; 
+const router = express.Router();
+
+router.get('/', productsController.getProducts);
+
+module.exports = router;
